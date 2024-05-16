925513d51b8699884873dfdd8ad2ccf0e2b629c2
Simran Chaurasia <simransia07@gmail.com>
2024-05-16 10:33:13 +0530

    fixed variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase';
-
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAVSuVvppjIFeRCZnMWjmr5CBDGp3Rbi14",
-    authDomain: "disney-plus-clone-97386.firebaseapp.com",
-    projectId: "disney-plus-clone-97386",
-    storageBucket: "disney-plus-clone-97386.appspot.com",
-    messagingSenderId: "753085384569",
-    appId: "1:753085384569:web:01bc7300cc7f46557111a8"
-  };
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
 
-  const firebaseApp= firebase.initializeApp(firebaseConfig)
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
-  const storage=firebase.storage()
- 
-  export {auth, provider, storage};
-  export default db;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
